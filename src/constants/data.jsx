56a4a4d7de48f2f4e7a9403ecbf62763275703fd
Nikolay Popov <niknikpopov@gmail.com>
2020-03-05 22:09:00 +0200

    g1: Initial commit

--- src/constants/data.jsx
+++ src/constants/data.jsx
@@ -1,14 +1,11 @@
-const data = [
-	{
-		level: 0,
-		speachPart: "verb",
-		imageURL: "../media/run.jpg",
-		en: "RUN",
-		de: "LAUF",
-		es: "CORRER",
-		ua: "БІГТИ"
-	},
+const data = [{
+    level: 0,
+    speachPart: "verb",
+    imageURL: "../media/run.jpeg",
+    en: "RUN",
+    de: "LAUF",
+    es: "CORRER",
+    ua: "БІГТИ"
+}, ];
 
-];
-
- export default data;
+export default data;
